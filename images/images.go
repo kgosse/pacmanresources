a2b2c148129232de89f3970dec3a28ff316c3c03
Kévin Gossé <gossekevin1@gmail.com>
2019-06-03 11:45:50 +0200

    export Images array

---
+++ images/images.go
@@ -0,0 +1,30 @@
+package images
+
+// Images is an array of all the images
+var Images = [...][]byte{
+	W0_png,
+	W1_png,
+	W2_png,
+	W3_png,
+	W4_png,
+	W5_png,
+	W6_png,
+	W7_png,
+	W8_png,
+	W9_png,
+	W10_png,
+	W11_png,
+	W12_png,
+	W13_png,
+	W14_png,
+	W15_png,
+	W16_png,
+	W17_png,
+	W18_png,
+	W19_png,
+	W20_png,
+	W21_png,
+	W22_png,
+	W23_png,
+	W24_png,
+}
